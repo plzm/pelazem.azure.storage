2038fe25b512d3c80e6a7b10c8b84eb56abb23b9
Patrick El-Azem <paelaz@microsoft.com>
2019-04-20 10:56:16 -0400

    2.4. Add queue functionality.

--- pelazem.azure.storage.app/Program.cs
+++ pelazem.azure.storage.app/Program.cs
@@ -7,20 +7,22 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using pelazem.util;
 
 namespace pelazem.azure.storage
 {
 	class Program
 	{
-		private static string _containerName = "test";
-		private static string _queueName = "";
 		private static string _accountName = "";
 		private static string _key = "";
+		private static string _containerName = "";
+		private static string _queueName = "";
 		private static string _policyName = "";
 
 		static void Main(string[] args)
 		{
-			Enqueue().Wait();
+			// Enqueue().Wait();
 
 			//UploadBlob().Wait();
 
@@ -28,19 +30,31 @@ namespace pelazem.azure.storage
 
 			// ProcessBlob(url).Wait();
 
+			Reprocess().Wait();
+
 			Console.WriteLine();
 			Console.WriteLine("Done. Press any key to exit.");
 			Console.ReadKey();
 		}
 
+		static async Task Reprocess()
+		{
+			ServiceClient storageClient = new ServiceClient();
+			StorageCredentials credentials = storageClient.GetStorageCredentials(_accountName, _key);
+			CloudStorageAccount storageAccount = storageClient.GetStorageAccount(credentials);
+
+			OpResult result = await storageClient.RetryPoisonQueueMessagesAsync(storageAccount, _queueName, 32);
+		}
+
 		static async Task Enqueue()
 		{
-			ServiceClient svc = new ServiceClient();
+			ServiceClient storageClient = new ServiceClient();
 
-			StorageCredentials credentials = svc.GetStorageCredentials(_accountName, _key);
-			CloudStorageAccount storageAccount = svc.GetStorageAccount(credentials);
+			StorageCredentials credentials = storageClient.GetStorageCredentials(_accountName, _key);
+			CloudStorageAccount storageAccount = storageClient.GetStorageAccount(credentials);
+			CloudQueue queue = (await storageClient.GetQueueAsync(storageAccount, _queueName, true)).Output;
 
-			var result = await svc.EnqueueAsync(storageAccount, _queueName, "This is a test");
+			var result = await storageClient.EnqueueMessageAsync(storageAccount, queue, "This is a test");
 		}
 
 		static async Task UploadBlob()
@@ -53,20 +67,23 @@ namespace pelazem.azure.storage
 			var result = await svc.UploadStringAsync(storageAccount, _containerName, "This is a test", "foo.txt");
 		}
 
-		static async Task ListBlobs()
+		static async Task<IList<ICloudBlob>> ListBlobs()
 		{
 			ServiceClient svc = new ServiceClient();
 
 			StorageCredentials credentials = svc.GetStorageCredentials(_accountName, _key);
 			CloudStorageAccount storageAccount = svc.GetStorageAccount(credentials);
 
-			List<ICloudBlob> blobsRaw = (await svc.ListBlobs(storageAccount, _containerName)).ToList();
-			List<string> sasUrls = new List<string>();
+			List<ICloudBlob> blobs = (await svc.ListBlobs(storageAccount, _containerName, "root/sub/folder/", 1000)).ToList();
 
-			foreach (ICloudBlob blob in blobsRaw)
-				sasUrls.Add(await svc.GetBlobSAPUrlFromBlobAsync(blob, _policyName));
+			//List<string> sasUrls = new List<string>();
 
-			Debug.WriteLine(sasUrls.Count);
+			//foreach (ICloudBlob blob in blobs)
+			//	sasUrls.Add(await svc.GetBlobSAPUrlFromBlobAsync(blob, _policyName));
+
+			//Debug.WriteLine(sasUrls.Count);
+
+			return blobs;
 		}
 
 		static async Task ProcessBlob(string url)
--- pelazem.azure.storage/ServiceClient.cs
+++ pelazem.azure.storage/ServiceClient.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
@@ -165,7 +166,7 @@ namespace pelazem.azure.storage
 
 		#region List Blobs
 
-		public async Task<IEnumerable<ICloudBlob>> ListBlobs(CloudStorageAccount storageAccount, string containerName)
+		public async Task<IEnumerable<ICloudBlob>> ListBlobs(CloudStorageAccount storageAccount, string containerName, string blobNamePrefix = "", int? maxResultsPerSegment = 50)
 		{
 			CloudBlobContainer container = await this.GetContainerAsync(storageAccount, containerName, true);
 
@@ -175,12 +176,11 @@ namespace pelazem.azure.storage
 
 			do
 			{
-				var response = await container.ListBlobsSegmentedAsync(string.Empty, true, BlobListingDetails.Metadata, 50, token, null, null);
+				var response = await container.ListBlobsSegmentedAsync(blobNamePrefix, true, BlobListingDetails.Metadata, maxResultsPerSegment, token, null, null);
 
 				token = response.ContinuationToken;
 
-				foreach (var blob in response.Results.Where(b => b is ICloudBlob))
-					result.Add(blob as ICloudBlob);
+				result.AddRange(response.Results.Where(b => b is ICloudBlob).Select(b => b as ICloudBlob));
 			}
 			while (token != null);
 
@@ -491,50 +491,234 @@ namespace pelazem.azure.storage
 
 		#region Queue
 
-		public async Task<OpResult> EnqueueAsync(CloudStorageAccount storageAccount, string queueName, string message, TimeSpan? timeToLiveOnQueue = null, TimeSpan? timeBeforeVisible = null)
+		public async Task<OpResult<CloudQueue>> GetQueueAsync(CloudStorageAccount storageAccount, string queueName, bool createIfNotExists = true)
 		{
-			OpResult result = new OpResult() { Succeeded = false };
+			OpResult<CloudQueue> result = new OpResult<CloudQueue>();
 
-			if (storageAccount == null)
-			{
-				result.Message = $"Parameter {nameof(storageAccount)} was null.";
+			result.ValidationResult.Validations.AddItems(new[] { ValidateStorageAccount(storageAccount), ValidateQueueName(queueName) });
+
+			if (!(result.ValidationResult.IsValid))
 				return result;
-			}
-
-			if (string.IsNullOrWhiteSpace(queueName))
-			{
-				result.Message = $"Parameter {nameof(queueName)} was empty or whitespace.";
-				return result;
-			}
-
-			if (string.IsNullOrWhiteSpace(message))
-			{
-				result.Message = $"Parameter {nameof(message)} was empty or whitespace.";
-				return result;
-			}
-
-			CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
-
-			CloudQueue queue = queueClient.GetQueueReference(queueName);
 
 			try
 			{
-				await queue.CreateIfNotExistsAsync();
+				CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
 
-				await queue.AddMessageAsync(new CloudQueueMessage(message), timeToLiveOnQueue, timeBeforeVisible, null, null);
+				CloudQueue queue = queueClient.GetQueueReference(queueName);
+
+				if (createIfNotExists)
+					await queue.CreateIfNotExistsAsync();
+
+				result.Output = queue;
+				result.Succeeded = true;
+			}
+			catch (Exception ex)
+			{
+				result.Exception = ex;
+				result.Output = null;
+				result.Succeeded = false;
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Wraps https://docs.microsoft.com/en-us/dotnet/api/microsoft.windowsazure.storage.queue.cloudqueue.getmessagesasync.
+		/// 
+		/// NOTE: this retrieves messages but DOES NOT DELETE them from the queue. The retrieved messages are marked invisible for the default invisibility timeout.
+		/// You - YES YOU - MUST explicitly delete messages off the queue so they do not "reappear".
+		/// </summary>
+		/// <param name="storageAccount"></param>
+		/// <param name="queueName"></param>
+		/// <param name="messageCount">Between 1 and 32. If outside this range, will be corrected internally to 32.</param>
+		/// <returns></returns>
+		public async Task<OpResult<List<CloudQueueMessage>>> GetQueueMessagesAsync(CloudStorageAccount storageAccount, CloudQueue queue, int messageCount = 32)
+		{
+			if (messageCount <= 0 || messageCount > 32)
+				messageCount = 32;
+
+			OpResult<List<CloudQueueMessage>> result = new OpResult<List<CloudQueueMessage>>();
+
+			result.ValidationResult.Validations.AddItems(new[] { ValidateStorageAccount(storageAccount), ValidateQueue(queue) });
+
+			if (!(result.ValidationResult.IsValid))
+				return result;
+
+			try
+			{
+				result.Output = (await queue.GetMessagesAsync(messageCount)).ToList();
+				result.Succeeded = true;
+			}
+			catch (Exception ex)
+			{
+				result.Succeeded = false;
+				result.Exception = ex;
+			}
+
+			return result;
+		}
+
+		public async Task<OpResult> EnqueueMessageAsync(CloudStorageAccount storageAccount, CloudQueue queue, string queueMessage, TimeSpan? timeToLiveOnQueue = null, TimeSpan? timeBeforeVisible = null)
+		{
+			OpResult result = new OpResult();
+
+			result.ValidationResult.Validations.AddItems(new[] { ValidateQueueMessage(queueMessage) });
+
+			if (!result.ValidationResult.IsValid)
+				return result;
+
+			CloudQueueMessage msg = new CloudQueueMessage(queueMessage);
+
+			return await EnqueueMessageAsync(storageAccount, queue, msg, timeToLiveOnQueue, timeBeforeVisible);
+		}
+
+		public async Task<OpResult> EnqueueMessageAsync(CloudStorageAccount storageAccount, CloudQueue queue, CloudQueueMessage queueMessage, TimeSpan? timeToLiveOnQueue = null, TimeSpan? timeBeforeVisible = null)
+		{
+			OpResult result = new OpResult();
+
+			result.ValidationResult.Validations.AddItems(new[] { ValidateStorageAccount(storageAccount), ValidateQueue(queue), ValidateQueueMessage(queueMessage) });
+
+			if (!result.ValidationResult.IsValid)
+				return result;
+
+			try
+			{
+				await queue.AddMessageAsync(queueMessage, timeToLiveOnQueue, timeBeforeVisible, null, null);
 
 				result.Succeeded = true;
 			}
 			catch (Exception ex)
 			{
-				// TODO Log Exception
-
-				// TODO log exception
-
 				result.Succeeded = false;
-				result.Message = "Error! Exception caught. See Output property for Exception.";
-				result.Output = ex;
+				result.Exception = ex;
 			}
+
+			return result;
+		}
+
+		public async Task<OpResult> DeleteMessageAsync(CloudStorageAccount storageAccount, CloudQueue queue, CloudQueueMessage queueMessage)
+		{
+			OpResult result = new OpResult() { Succeeded = false } ;
+
+			result.ValidationResult.Validations.AddItems(new[] { ValidateStorageAccount(storageAccount), ValidateQueue(queue), ValidateQueueMessage(queueMessage) });
+
+			if (!result.ValidationResult.IsValid)
+				return result;
+
+			try
+			{
+				await queue.DeleteMessageAsync(queueMessage);
+
+				result.Succeeded = true;
+			}
+			catch (Exception ex)
+			{
+				result.Succeeded = false;
+				result.Exception = ex;
+			}
+
+			return result;
+		}
+
+		public async Task<OpResult> DeleteMessagesAsync(CloudStorageAccount storageAccount, CloudQueue queue, IEnumerable<CloudQueueMessage> queueMessages)
+		{
+			OpResult result = new OpResult() { Succeeded = false };
+
+			result.ValidationResult.Validations.AddItems(new[] { ValidateStorageAccount(storageAccount), ValidateQueue(queue), ValidateQueueMessages(queueMessages) });
+
+			if (!result.ValidationResult.IsValid)
+				return result;
+
+			try
+			{
+				foreach(CloudQueueMessage queueMessage in queueMessages.Where(qm => qm != null))
+					await queue.DeleteMessageAsync(queueMessage);
+
+				result.Succeeded = true;
+			}
+			catch (Exception ex)
+			{
+				result.Succeeded = false;
+				result.Exception = ex;
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Removes all messages from the poison queue and tries them again. Processes batches of size batchSize.
+		/// </summary>
+		/// <param name="storageAccount"></param>
+		/// <param name="queueName">The real queue - NOT the poison queue</param>
+		/// <param name="batchSize">Number of messages to process at a time. Between 1 and 32.</param>
+		/// <returns></returns>
+		public async Task<OpResult> RetryPoisonQueueMessagesAsync(CloudStorageAccount storageAccount, string queueName, int batchSize)
+		{
+			OpResult result = new OpResult();
+
+			OpResult<CloudQueue> queueResult = await GetQueueAsync(storageAccount, queueName, false);
+
+			result.ValidationResult.Validations.AddItems(queueResult.ValidationResult.Validations);
+
+			if (!result.ValidationResult.IsValid)
+				return result;
+
+			CloudQueue queue = null;
+
+			if (queueResult.Succeeded && queueResult.Output != null)
+				queue = queueResult.Output;
+			else
+			{
+				result.Message = "Queue not found.";
+				return result;
+			}
+
+			string poisonQueueName = queueName + "-poison";
+			CloudQueue poisonQueue = null;
+
+			OpResult<CloudQueue> poisonQueueResult = await GetQueueAsync(storageAccount, poisonQueueName, false);
+
+			if (poisonQueueResult.Succeeded && poisonQueueResult.Output != null)
+				poisonQueue = poisonQueueResult.Output;
+			else
+			{
+				result.Message = "Poison Queue not found.";
+				return result;
+			}
+
+			// We now have the queue and the poison queue
+			List<CloudQueueMessage> messagesToDeleteFromPoisonQueue = new List<CloudQueueMessage>();
+
+			while (true)
+			{
+				messagesToDeleteFromPoisonQueue.Clear();
+
+				List<CloudQueueMessage> poisonQueueMessages = null;
+
+				OpResult<List<CloudQueueMessage>> poisonQueueMessagesResult = await GetQueueMessagesAsync(storageAccount, poisonQueue, batchSize);
+
+				if (poisonQueueMessagesResult.Succeeded)
+					poisonQueueMessages = poisonQueueMessagesResult.Output;
+
+				if (poisonQueueMessages == null || poisonQueueMessages.Count == 0)
+					break;
+
+				foreach (CloudQueueMessage poisonQueueMessage in poisonQueueMessages)
+				{
+					CloudQueueMessage retryMessage = new CloudQueueMessage(poisonQueueMessage.AsString);
+
+					OpResult retryEnqueueResult = await EnqueueMessageAsync(storageAccount, queue, retryMessage);
+
+					if (retryEnqueueResult.Succeeded)
+						messagesToDeleteFromPoisonQueue.Add(poisonQueueMessage);
+				}
+
+				OpResult deleteResult = await DeleteMessagesAsync(storageAccount, poisonQueue, messagesToDeleteFromPoisonQueue);
+
+				// TODO Should we sleep the thread?
+			}
+
+			result.Succeeded = true;
 
 			return result;
 		}
@@ -573,6 +757,124 @@ namespace pelazem.azure.storage
 			}
 
 			return storageAccount;
+		}
+
+		#endregion
+
+		#region Validation
+
+		public Validation ValidateStorageAccount(CloudStorageAccount storageAccount)
+		{
+			Validation result = new Validation();
+
+			if (storageAccount == null)
+			{
+				result.Message = $"{nameof(storageAccount)} was null, empty, or whitespace.";
+				result.IsValid = false;
+			}
+			else
+			{
+				result.Message = $"{nameof(storageAccount)} valid.";
+				result.IsValid = true;
+			}
+
+			return result;
+		}
+
+		public Validation ValidateQueue(CloudQueue queue)
+		{
+			Validation result = new Validation();
+
+			if (queue == null)
+			{
+				result.Message = $"{nameof(queue)} was null.";
+				result.IsValid = false;
+			}
+			else
+			{
+				result.Message = $"{nameof(queue)} valid.";
+				result.IsValid = true;
+			}
+
+			return result;
+		}
+
+		public Validation ValidateQueueName(string queueName)
+		{
+			Validation result = new Validation();
+
+			if (string.IsNullOrWhiteSpace(queueName))
+			{
+				result.Message = $"{nameof(queueName)} was null or whitespace.";
+				result.IsValid = false;
+			}
+			else
+			{
+				result.Message = $"{nameof(queueName)} valid.";
+				result.IsValid = true;
+			}
+
+			return result;
+		}
+
+		public Validation ValidateQueueMessage(string queueMessage)
+		{
+			Validation result = new Validation();
+
+			if (string.IsNullOrWhiteSpace(queueMessage))
+			{
+				result.Message = $"{nameof(queueMessage)} was null, empty, or whitespace.";
+				result.IsValid = false;
+			}
+			else if ((queueMessage.Length * sizeof(Char)) > 64000)
+			{
+				// Storage queue message length max is 64KiB
+				result.Message = $"{nameof(queueMessage)} lwngth exceeds 64 KiB, which is the Azure storage queue max size.";
+				result.IsValid = false;
+			}
+			else
+			{
+				result.Message = $"{nameof(queueMessage)} valid.";
+				result.IsValid = true;
+			}
+
+			return result;
+		}
+
+		public Validation ValidateQueueMessage(CloudQueueMessage queueMessage)
+		{
+			Validation result = new Validation();
+
+			if (queueMessage == null)
+			{
+				result.Message = $"{nameof(queueMessage)} was null.";
+				result.IsValid = false;
+			}
+			else
+			{
+				result.Message = $"{nameof(queueMessage)} valid.";
+				result.IsValid = true;
+			}
+
+			return result;
+		}
+
+		public Validation ValidateQueueMessages(IEnumerable<CloudQueueMessage> queueMessages)
+		{
+			Validation result = new Validation();
+
+			if (queueMessages == null)
+			{
+				result.Message = $"{nameof(queueMessages)} was null.";
+				result.IsValid = false;
+			}
+			else
+			{
+				result.Message = $"{nameof(queueMessages)} valid.";
+				result.IsValid = true;
+			}
+
+			return result;
 		}
 
 		#endregion
