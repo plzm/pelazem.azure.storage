a43e277f6a3557ca269bd0efad083ba13c0d21ca
paelaz@microsoft.com <paelaz@microsoft.com>
2019-12-31 12:55:44 -0500

    Add validator unit test

--- pelazem.azure.storage.tests/ValidatorTests.cs
+++ pelazem.azure.storage.tests/ValidatorTests.cs
@@ -112,5 +112,21 @@ namespace pelazem.azure.storage.tests
 			Assert.False(validationResult.IsValid);
 		}
 
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		public void ValidateQueueNameShouldReturnFalseForNullOrWhitespace(string value)
+		{
+			// Arrange
+
+			// Act
+			ValidationResult validationResult = Validator.ValidateQueueName(value);
+
+			// Assert
+			Assert.Equal(1, validationResult.Validations.Count);
+			Assert.False(validationResult.Validations[0].IsValid);
+			Assert.False(validationResult.IsValid);
+		}
+
 	}
 }
